8df77b3354fa6517bc0005d3a5a718e4cb7ccdbf
Tyler Bainbridge <tyler_bainbridge@student.uml.edu>
2016-02-10 16:00:23 -0500

    second commit

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ var debug = require('debug')('passport-mongo'),
     app = require('./app');
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000){
+  console.log('listening on', http.address().port);
+});
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
